400427f83f234c5de27c4a8f3603b425840dd2c8
pizzadestroyer1 <atepa5000@gmail.com>
2021-04-08 22:31:57 +0700

    test1

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(w, "World")
+	})
+
+	http.ListenAndServe(":"+port, nil)
+}
